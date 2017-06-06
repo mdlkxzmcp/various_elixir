b7b71db831908a4a43315703ea89d41c55ff894a
Maximiliaan Strother III <maxbax@op.pl>
2017-06-06 03:20:23 +0200

    Init of example_elmchemy
    As done in https://hackernoon.com/elmchemy-write-type-safe-elixir-code-with-elms-syntax-part-1-introduction-8968b76d721d

---
+++ Plain/learning/example_elmchemy/lib/example_elmchemy.ex
@@ -0,0 +1,18 @@
+defmodule ExampleElmchemy do
+  @moduledoc """
+  Documentation for ExampleElmchemy.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExampleElmchemy.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ Plain/learning/example_elmchemy/test/example_elmchemy_test.exs
@@ -0,0 +1,8 @@
+defmodule ExampleElmchemyTest do
+  use ExUnit.Case
+  doctest ExampleElmchemy
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
